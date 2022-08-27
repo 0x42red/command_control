891760f92df639033b159428650ad1645586c323
0x42 <root@0x42.red>
2022-08-26 20:32:49 -0700

    Updating cmd diractory

---
+++ cmd/command/command_server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/0x42red/command_control/pkg/embedkeys"
+	"github.com/0x42red/command_control/pkg/server"
+	"github.com/0x42red/command_control/pkg/ui"
+	"github.com/jroimartin/gocui"
+
+	gossh "golang.org/x/crypto/ssh"
+)
+
+func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(ui.Layout)
+	g.Cursor = true
+	go func(g *gocui.Gui) {
+		for range time.Tick(1 * time.Second) {
+			if g.Cursor {
+				g.Cursor = false
+			} else {
+				g.Cursor = true
+			}
+		}
+	}(g)
+
+	publicKey, err := embedkeys.GetPublicKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	commandServer := &server.CommandServer{
+		AllowedKeys: []gossh.PublicKey{
+			publicKey,
+		},
+	}
+	go commandServer.Start()
+
+	err = ui.SetKeyBinding(g, commandServer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go ui.UpdateCommandServerGUI(g, commandServer)
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Fatal(err)
+	}
+}
